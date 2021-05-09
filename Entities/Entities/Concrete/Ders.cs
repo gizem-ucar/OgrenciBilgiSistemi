f7ad2147910b00b9a68f1d7806f51d2b6c029fa4
gizem-ucar <77545358+gizem-ucar@users.noreply.github.com>
2021-05-09 21:58:39 +0300

    g

---
+++ Entities/Entities/Concrete/Ders.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Ders:IEntity
+    {
+        public string DersId { get; set; }
+        public string DersAdi { get; set; }
+        public string Sube { get; set; }
+    }
+}
---
+++ Entities/Entities/Concrete/Devamsizlik.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Devamsizlik:IEntity
+    {
+        public int OgrenciId { get; set; }
+        public string DevamsizlikId { get; set; }
+        public string DersId { get; set; }
+        public string DevamsizlikDurumu { get; set; }
+    }
+}
