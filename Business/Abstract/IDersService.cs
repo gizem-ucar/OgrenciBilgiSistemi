1141485afb32047138f22ff3e736fc0952fd0087
gizem-ucar <77545358+gizem-ucar@users.noreply.github.com>
2021-05-11 18:44:54 +0300

    g

---
+++ Business/Abstract/IDersService.cs
@@ -0,0 +1,19 @@
+﻿using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IDersService
+    {
+        IDataResult<Ders> GetById(string Id);
+        IDataResult<List<Ders>> GetAll();
+        IResult Add(Ders ders);
+        IResult Delete(Ders ders);
+        IResult Update(Ders ders);
+
+    }
+}
